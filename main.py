4432504dfebcff4ff77ea2dd08a74ee85b9adf9d
makamadams2008-wq <makam.adams.2008@gmail.com>
2026-04-23 12:09:09 +1200

    Root Class created
    Created the root class and added a file docstring.

---
+++ main.py
@@ -0,0 +1,10 @@
+"""This file is for the frontend tkinter aplication where the root lives
+and all of the windows and frames asosiated with it. This file takes sorted
+data from the superbase database and diplays it to the user."""
+
+class Root:
+    def __init__(self):
+        self.tabs = []
+
+        self.maindashboard = None
+
