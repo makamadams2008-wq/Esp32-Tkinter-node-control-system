1f878ee98cf3618401b505aed98d9c6ebaa6ce11
makamadams2008-wq <makam.adams.2008@gmail.com>
2026-04-23 13:23:27 +1200

    CommandApp created

--- main.py
+++ main.py
@@ -2,9 +2,24 @@
 and all of the windows and frames asosiated with it. This file takes sorted
 data from the superbase database and diplays it to the user."""
 
-class Root:
-    def __init__(self):
+import tkinter as tk
+import  helper_functions as hf
+import constants as const
+
+
+class ComandApp:
+    def __init__(self, parent):
         self.tabs = []
 
         self.maindashboard = None
 
+
+if __name__ == "__main__":
+    """Main."""
+    root = tk.Tk()
+    root.title("Main Game")
+    root.withdraw()
+    
+    comand_app = ComandApp(hf.config_root(root)) # Configs the comand app
+
+    root.mainloop()
