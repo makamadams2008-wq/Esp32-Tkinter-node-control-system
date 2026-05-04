c516035360809eb7d6d2025615ebd8976067f3ad
makamadams2008-wq <makam.adams.2008@gmail.com>
2026-05-05 11:00:08 +1200

    Data is coming in correctly but unformated

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
+import time
 from supabase import create_client
 
 url = "https://dvukxjndnkoqvjqpueqa.supabase.co"
 key = "sb_publishable_MTMJMC0SHyjp0qxjPjRwcA_W-cZaxcI"
 supabase = create_client(url, key)
-response = supabase.table("Devices").select("*, Components(*)").execute()
+response = supabase.table("Devices").select("Components(*)").execute()
 print(response)
