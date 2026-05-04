20d9f1ba9f6599ef2f7fe972c9bb157e420817e4
makamadams2008-wq <makam.adams.2008@gmail.com>
2026-05-04 17:58:20 +1200

    Started addding the new window for updating the board state

--- components/update_status_pop_up.py
+++ components/update_status_pop_up.py
@@ -8,5 +8,18 @@ class UpdatePopUp(tk.Frame):
         super().__init__(parent) # Inheritance parent data
         self.controller = controller
         print(device["device_name"])
-        self.system_status_frame = hf.config_frame(parent, 1, 4, True, 2, 0, True, const.MIDGROUND_COLOR)
-        self.status_label = hf.create_label(parent=self.system_status_frame, message="Status: Stable connection", pos_x=0, pos_y=2, bg_color=const.BACKGROUND_COLOR)
+        self.update_status_frame = hf.config_frame(parent, 1, 3, True, 0, 0, True, const.MIDGROUND_COLOR)
+
+        self.outputs_label = hf.create_label(parent=self.update_status_frame, message="Outputs", pos_x=0, pos_y=0, bg_color=const.BACKGROUND_COLOR)
+
+        
+        self.led_frame = hf.config_frame(self.update_status_frame, 1, 2, True, 1, 0, True, const.MIDGROUND_COLOR)
+        self.leds_label = hf.create_label(parent=self.led_frame, message="Leds", pos_x=0, pos_y=1, bg_color=const.BACKGROUND_COLOR)
+        
+        self.motor_frame = hf.config_frame(self.update_status_frame, 1, 2, True, 2, 0, True, const.MIDGROUND_COLOR)
+        self.motor_label = hf.create_label(parent=self.motor_frame, message="Motor", pos_x=0, pos_y=2, bg_color=const.BACKGROUND_COLOR)
+        self.set_motor_direction_input = hf.create_entry(parent=self.motor_frame, message="Please pick a motor direction in digrees", func=self.on_update, pos_x=0, pos_y=3, bg_color=const.BACKGROUND_COLOR)
+
+    def on_update():
+        pass
+        
